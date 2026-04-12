b7d4772318f69ef178327ddb7bd8bd932689ef14
Nguyen Trung <nguyentrungyt284@gmail.com>
2026-04-12 15:17:23 +0700

    push .env

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ export async function predictNews(payload: {
   text?: string;
   url?: string;
 }) {
-  const res = await fetch("http://127.0.0.1:8000/api/predict/", {
+  const res = await fetch(`${process.env.API}` + "/api/predict/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
